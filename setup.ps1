d7dedef82c94551bd5054a7de4d71c28717a3267
Yayes-Yasta <155449161+Yayes-Yasta@users.noreply.github.com>
2026-05-13 18:02:02 +0400

    Create setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,12 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Install Chocolatey
+[System.Net.ServicePointManager]::SecurityProtocol = `
+[System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install common tools
+choco install -y git mingw make python vscode 7zip
+
+# GCC is included in mingw
